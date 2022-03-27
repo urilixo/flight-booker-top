04e382dc2815974842c675ab21ed038df9f08b15
urilixo <falecomelvio@hotmail.com>
2022-03-27 17:15:48 -0300

    Migrates db

--- db/migrate/20220327191124_create_flights.rb
+++ db/migrate/20220327191124_create_flights.rb
@@ -1,10 +1,10 @@
 class CreateFlights < ActiveRecord::Migration[6.1]
   def change
     create_table :flights do |t|
-      t.references :departure_airport, null: false, foreign_key: true
-      t.references :arrival_aiport, null: false, foreign_key: true
+      t.references :departure_airport, null: false, foreign_key: {to_table: :airports}
+      t.references :arrival_aiport, null: false, foreign_key: {to_table: :airports}
       t.datetime :departure_time
-      t.int :duration
+      t.integer :duration
 
       t.timestamps
     end
