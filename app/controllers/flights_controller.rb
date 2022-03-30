cb6fe40a9360ccc24c2c0afa0a908168327f76db
urilixo <falecomelvio@hotmail.com>
2022-03-30 05:15:41 -0300

    Add allowed parameters to flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -65,6 +65,6 @@ class FlightsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def flight_params
-      params.require(:flight).permit(:index, :show)
+      params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :departure_time, :duration)
     end
 end
